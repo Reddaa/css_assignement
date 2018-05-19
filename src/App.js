18735d91d9f503f246b5e1dee55937f25c92e8f3
Redaa <164438@supinfo.com>
2018-05-19 18:30:22 +0000

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+          <p>First commit</p>
+      </div>
+    );
+  }
+}
+
+export default App;
